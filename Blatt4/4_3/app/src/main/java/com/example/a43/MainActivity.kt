43ea8f49f053f0b2bca61f0b977166237e5be60d
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-04-25 15:40:27 +0200

    forward and backword button
    Implemented forward and backward buttons

--- Blatt4/4_3/app/src/main/java/com/example/a43/MainActivity.kt
+++ Blatt4/4_3/app/src/main/java/com/example/a43/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.a43
 
 import android.os.Bundle
-import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
@@ -17,24 +16,31 @@ class MainActivity : AppCompatActivity() {
 
         val webView = findViewById<WebView>(R.id.webView)
         val textInput = findViewById<EditText>(R.id.textInput)
-        val button = findViewById<Button>(R.id.button)
+        val btn_search = findViewById<Button>(R.id.button)
+        val btn_backward = findViewById<Button>(R.id.btn_back)
+        val btn_forward = findViewById<Button>(R.id.btn_forward)
+
 
         val searchQString = "https://www.google.at/search?hl=de&q="
 
 
         webView.webViewClient = WebViewClient()
 
-        webView.loadUrl("https://www.google.at/")
-        button.setOnClickListener{
+        webView.loadUrl("https://www.wikipedia.org/")
+        btn_search.setOnClickListener{
             val input = textInput.text.toString()
             if (!(input.startsWith("http")) && !(input.startsWith("www"))) {
                 webView.loadUrl(searchQString + input)
             } else {
                 webView.loadUrl("https://" + input)
             }
+        }
 
-
-
+        btn_backward.setOnClickListener{
+            if(webView.canGoBack()) webView.goBack()
+        }
+        btn_forward.setOnClickListener{
+            if(webView.canGoForward()) webView.goForward()
         }
     }
 
