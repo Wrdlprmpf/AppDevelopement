b459d2b033d7cc0723e73232ae8ad4b749149a53
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-05-02 19:46:47 +0200

    Chat App update...

--- Blatt5/5_1/app/src/main/java/com/example/a51/Adapter.kt
+++ Blatt5/5_1/app/src/main/java/com/example/a51/Adapter.kt
@@ -1,25 +1,59 @@
 package com.example.a51
 
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
+import android.widget.TextView
 
 
-class Adapter(var data:ArrayList<Message>) : RecyclerView.Adapter<ViewHolder>() {
+class Adapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var sentmsg = 1
+    var recievemsg = 2
+    lateinit var messages: ArrayList<Message>
 
-
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when(viewType){
+            sentmsg -> {
+                return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.message_outgoing, parent, false))
+            }
+            recievemsg -> {
+                return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.message_incoming, parent, false))
+            }
+            else -> {
+                return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.message_incoming, parent,false))
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is ViewHolder ->{
+                holder.connect(messages[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return messages.size
     }
 
+    fun loadList(messages: ArrayList<Message>){
+        this.messages = messages
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val sender: TextView = itemView.findViewById(R.id.messageSender)
+        val date: TextView = itemView.findViewById(R.id.messageDate)
+        val message: TextView = itemView.findViewById(R.id.messageText)
+
+        fun connect(msg: Message){
+            sender.text = msg.sender
+            date.text = msg.date
+            message.text = msg.message
+        }
+    }
 
 }
--- Blatt5/5_1/app/src/main/java/com/example/a51/MainActivity.kt
+++ Blatt5/5_1/app/src/main/java/com/example/a51/MainActivity.kt
@@ -5,24 +5,33 @@ import android.os.Bundle
 import android.util.Log
 import java.util.*
 import android.util.Log.*
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.logging.Level.INFO
 
 class MainActivity : AppCompatActivity() {
     private
     var d = Date(2022,10,10)
-    var test = Message("Ivan","Hallo",false)
 
     var messages = ArrayList<Message>()
-    val list:RecyclerView = findViewById(R.id.recyclerView)
+    lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<RecyclerView>(R.id.recyclerView)
+
         messages.add(Message("Ivan","Wg",false))
         messages.add(Message("Ivan","yo",false))
         messages.add(Message("Ivan","dude",false))
         messages.add(Message("Ivan","eyyyyy",false))
-        messages.add(Message("Ivan","ok",false))
+        messages.add(Message("Ivan","ok",true))
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = Adapter()
+        recyclerView.adapter = adapter
+
+        adapter.loadList(this.messages)
     }
 }
