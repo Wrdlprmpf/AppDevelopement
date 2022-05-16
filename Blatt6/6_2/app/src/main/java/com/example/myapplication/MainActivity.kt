e8cccee27877cc873a677f4d11f6d27f488232a1
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-05-16 16:05:04 +0200

    Refactored Conversion App

--- Blatt6/6_2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Blatt6/6_2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,7 +12,7 @@ import android.widget.*
 import androidx.appcompat.app.ActionBar
 import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     var units = arrayOf<String>(
         "centimeter to inch",
@@ -24,27 +24,12 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     var fragmentOne = Fragment()
     var fragmentTwo = FragmentTwo()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //ActionBar aufrufen
         val actionBar = supportActionBar
         actionBar!!.title = "FragmentApp"
-
-        val spin = findViewById<Spinner>(R.id.spinner)
-        spin.onItemSelectedListener = this
-
-        val adapter: ArrayAdapter<*> =
-            ArrayAdapter<Any>(this, android.R.layout.simple_spinner_item, units)
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spin.adapter = adapter
-
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            conversion()
-        }
 
         fragmentManagement(1)
     }
@@ -59,11 +44,10 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             fragmentTransaction.replace(R.id.viewGroup, fragmentOne)
 
         } else if (type == 2) {
-            fragmentTransaction.replace(R.id.viewGroup,fragmentTwo)
+            fragmentTransaction.replace(R.id.viewGroup, fragmentTwo)
         }
 
         fragmentTransaction.commit()
-
     }
 
     // method to inflate the options menu when
@@ -78,35 +62,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             R.id.fragOneMenu -> fragmentManagement(1)
             R.id.fragTwoMenu -> fragmentManagement(2)
         }
-
-
         return super.onOptionsItemSelected(item)
-    }
-
-    var pos = 0;
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        Toast.makeText(applicationContext, units[position], Toast.LENGTH_SHORT).show()
-        Log.i("InfoPosition", position.toString())
-        pos = position
-        conversion()
-    }
-
-    fun conversion() {
-        val inputText = findViewById<EditText>(R.id.number1)
-        val outputText = findViewById<TextView>(R.id.number2)
-
-        when (pos) {
-            0 -> outputText.setText((inputText.text.toString().toInt() / 2.54).toString())
-            1 -> outputText.setText((inputText.text.toString().toInt() * 12).toString())
-            2 -> outputText.setText((inputText.text.toString().toInt() * 1.609).toString())
-            3 -> outputText.setText((inputText.text.toString().toInt() * 1.852).toString())
-            4 -> outputText.setText((inputText.text.toString().toInt() / 1.094).toString())
-        }
-
-    }
-
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-
     }
 }
